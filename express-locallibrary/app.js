350be3c4a86a50579f5d94dae03b7df936c6488a
Rockleemode <96262404+Rockleemode@users.noreply.github.com>
2023-12-06 14:49:47 +0100

    added models for the books and populated it

--- express-locallibrary/app.js
+++ express-locallibrary/app.js
@@ -9,6 +9,17 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
+// Set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = "mongodb+srv://rocklee:rocklee@rocklee.utzths0.mongodb.net/local-library?retryWrites=true&w=majority";
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
--- express-locallibrary/routes/users.js
+++ express-locallibrary/routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get("/cool", (req, res) =>{
+  res.send("you're so cool")
+})
+
+
 module.exports = router;
