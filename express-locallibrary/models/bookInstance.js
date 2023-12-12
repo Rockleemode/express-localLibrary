c876dadf8d6a28a1dc4a8612f766e7fe2b89ba74
Rockleemode <96262404+Rockleemode@users.noreply.github.com>
2023-12-12 14:08:32 +0100

    added book and bookinstance for with author delete option

--- express-locallibrary/models/bookInstance.js
+++ express-locallibrary/models/bookInstance.js
@@ -15,6 +15,12 @@ const BookInstanceSchema = new Schema({
   due_back: { type: Date, default: Date.now },
 });
 
+//formatted date as YYYY-MM-DD
+BookInstanceSchema.virtual("due_back_yyyy_mm_dd").get(function () {
+  return DateTime.fromJSDate(this.due_back).toISODate(); // format 'YYYY-MM-DD'
+});
+
+
 //formatted date using luxon.
 BookInstanceSchema.virtual("due_back_formatted").get(function () {
   return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
